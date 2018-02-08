8503576f0419ba73a83fb5737dd25f1360756755
Orcthanc <orcthanclp@gmail.com>
2018-02-08 12:26:08 +0100

    + Added an Wireframe-mode on space

--- OpenGLDoWhatYouWant/Test/KeyboardHandler.cs
+++ OpenGLDoWhatYouWant/Test/KeyboardHandler.cs
@@ -4,21 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK.Input;
+using OpenTK.Graphics.OpenGL4;
 
 namespace OpenGLDoWhatYouWant
 {
     partial class Window
     {
+        private bool isWireframe;
+
         protected override void OnKeyDown(KeyboardKeyEventArgs e)
         {
-            Console.WriteLine("[INFO] Key " + e.Key + " got pressed...");
+            Console.WriteLine("[INFO] Key " + e.Key + " got pressed");
 
             switch (e.Key)
             {
                 case Key.Escape:
                     Close();
                     break;
-                
+                case Key.Space:
+                    if (isWireframe)
+                    {
+                        GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
+                        isWireframe = false;
+                    }
+                    else
+                    {
+                        GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+                        isWireframe = true;
+                    }
+                    break;
             }
         }
     }
